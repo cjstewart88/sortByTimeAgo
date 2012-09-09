049baad36a6f9a2b52b9c8f5c531ab96fbb3d4df
Cody Stewart <cjstewart88@gmail.com>
2012-09-09 00:35:04 -0400

    support for month and year.. also fixed a bug that didn't sort values like 'about an hour ago' or 'about a minute ago'

--- sortByTimeAgo.js
+++ sortByTimeAgo.js
@@ -7,7 +7,9 @@
       minutesAgo: [],
       hoursAgo:   [],
       daysAgo:    [],
-      weeksAgo:   []
+      weeksAgo:   [],
+      monthsAgo:  [],
+      yearsAgo:   []
     };
     
     $.each(data, function () {
@@ -16,11 +18,16 @@
       else if (this.timeAgo.indexOf("hour")   != -1) seperatedData.hoursAgo.push(this);
       else if (this.timeAgo.indexOf("day")    != -1) seperatedData.daysAgo.push(this);
       else if (this.timeAgo.indexOf("week")   != -1) seperatedData.weeksAgo.push(this);
+      else if (this.timeAgo.indexOf("month")  != -1) seperatedData.yearsAgo.push(this);
+      else if (this.timeAgo.indexOf("year")   != -1) seperatedData.monthsAgo.push(this);
     });
     
     $.each(seperatedData, function () {
       this.sort(function (a, b) {
-        return parseInt(a.timeAgo.match(/\d+/)[0]) - parseInt(b.timeAgo.match(/\d+/)[0]);
+        var aTimeAgo = a.timeAgo.match(/\d+/);
+        var bTimeAgo = b.timeAgo.match(/\d+/);
+
+        return parseInt(aTimeAgo == null ? 0 : aTimeAgo[0]) - parseInt(bTimeAgo == null ? 0 : bTimeAgo[0]);
       });
     });
     
@@ -28,6 +35,8 @@
                              seperatedData.minutesAgo, 
                              seperatedData.hoursAgo, 
                              seperatedData.daysAgo,
-                             seperatedData.weeksAgo);
+                             seperatedData.weeksAgo,
+                             seperatedData.monthsAgo,
+                             seperatedData.yearsAgo);
   };
 })(jQuery);
